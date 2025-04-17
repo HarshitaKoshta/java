70b44b0c6731b6d0f1fbcec9d746ac7f95a58c8b
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-04-17 20:28:40 +0530

    twosum

--- SumTwoD.java
+++ SumTwoD.java
@@ -1,6 +1,6 @@
 public class SumTwoD{
     public static void main(String[] args) {
-        int[][] arr={{2,4,6,3},{7,8,5,3}};
+        int[][] arr={{1,1,1,1},{1,1,1,3}};
         int sum = 0;
 
         for (int i = 0; i < arr.length; i++) {
