d494d2ca9eb78ae173d8005ac8e251d6b7822598
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-04-25 11:16:50 +0530

    return sum

---
+++ ReturnSum.java
@@ -0,0 +1,9 @@
+class ReturnSum{
+    static int sum(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int obj=sum(2,4);
+    System.out.println("sum:"+obj);
+    }
+}
