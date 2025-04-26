6f57701dc3bc579b94aba9ebfb0dcb724e71ae8e
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-04-26 18:35:19 +0530

    Method cube

---
+++ cube.java
@@ -0,0 +1,10 @@
+public class cube{
+    static void cube(int n){
+        int c = n*n*n;
+        System.out.println("cube:"+c);
+
+    }
+    public static void main(String[] args) {
+        cube(5);
+    }
+}
