7e7f44a0d61b0a956264e5c2ede6c91cb96a0650
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-04-26 17:57:42 +0530

    method

--- FindMissing.java
+++ FindMissing.java
@@ -2,13 +2,14 @@ class FindMissing{
     static void missing(){
         int[] a ={1,2,4,5,6,7};
         int n=7;
-        int sum =0;
-        int suma=0;
-    
+       int suma=0;
+       /* int sum =0;
         for (int i = 0; i <=n ; i++) {
             sum+=i;
        }
-       System.out.println("sum"+sum);
+      */
+        int sum = (n*(n+1))/2;
+         System.out.println("sum"+sum);
        for (int i = 0; i < a.length; i++) {
            suma = suma+a[i];
        }
---
+++ StringName.java
@@ -0,0 +1,10 @@
+
+
+class StringName{
+    static void Name(String name){
+        System.out.println("hello"+name);
+    }
+    public static void main(String[] args) {
+        Name("pihu");
+    }
+}
