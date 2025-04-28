23569102aa40415710e6273d4bfef5532896b1a0
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-04-28 12:23:51 +0530

    linear search through method

--- BubbleSort.java
+++ BubbleSort.java
@@ -15,7 +15,7 @@ class BubbleSort{
      }
 
     public static void main(String[] args) {
-        int[] arr = {2,3,1,5,12,51,6};
+        int[] arr = {2,3,3,1,5,12,51,6};
         bubbleSort(arr);
     }
 }
