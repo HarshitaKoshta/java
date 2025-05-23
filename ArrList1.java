55a8833a523fa81df6aa655e3a919856a2c27038
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-05-23 10:13:49 +0530

    array list

---
+++ ArrList1.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+class ArrList1{
+    public static void main(String[] args) {
+      ArrayList<String> arr = new ArrayList<>();
+      arr.add("harshita");
+      arr.add("vanshika");
+      arr.add("astha");
+      arr.add(0,"hello");
+      System.out.println(arr);
+    }
+}
