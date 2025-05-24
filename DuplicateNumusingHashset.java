4a9fb43089c99a837db5bdcfaf82b3001638d5c3
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-05-24 23:07:52 +0530

    duplicate num

--- DuplicateNumusingHashset.java
+++ DuplicateNumusingHashset.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 
 class DuplicateNumusingHashset{
     public static void main(String[] args) {
-        int[] a ={1,2,4,3,4};
+        int[] a ={1,2,4,3,4,1,2};
         HashSet<Integer> set = new HashSet<>();
 
         for(int num:a){
