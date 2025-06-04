eb185c159b5fe025c9e53ae51c5cee522bbd8c05
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-06-04 10:15:22 +0530

    runnable try catch

--- runnabl2.java
+++ runnabl2.java
@@ -4,13 +4,19 @@ class MyRunnable implements java.lang.Runnable{
     }
 }
 
-public class Runnable1 {
+
+public class runnabl2 {
     public static void main(String[] args) {
         MyRunnable m = new MyRunnable();
         Thread t = new Thread(m);
         t.start();
-          
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+
+        }for(int i=1;i<=5;i++){
+            System.out.println(i);
+          }
         System.out.println("main thread is running");
     }
 }
-
