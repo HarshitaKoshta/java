fdd1dc8e9421bda14ff896a68d8636c8609577d3
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-06-05 12:44:02 +0530

    thread without extra class

--- threadPrblm.java
+++ threadPrblm.java
@@ -7,24 +7,26 @@ class myThread extends Thread{
      
     }
     public void run(){
-          if(amt>balance){
-       System.out.println("paise ni h itne....hum gareeb h");
-    }else{
-        System.out.println("money is going to withdrawl:"+amt);
-        try {
-             Thread.sleep(1000);
-        } catch (Exception e) {
+        synchronized (myThread.class) {
+            if(balance>=amt){
+                System.out.println("money withdrawl:"+amt);
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {    }
+                    balance -=amt;
+                    System.out.println("remaining :"+balance);
+                }else{
+                    System.out.println("balance ni h itna");
+            }
         }
-       System.out.println("remaining balance:"+(balance-amt));
     }
- }
 }
 
 public class threadPrblm {
     public static void main(String[] args) {
-        myThread t = new myThread(800);
-         myThread t1 = new myThread(400);
-         t.start();
-         t1.start();
+        myThread t = new myThread(700);
+        myThread t1 = new myThread(400);
+        t.start();
+        t1.start();
     }
 }
