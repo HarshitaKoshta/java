5e539d23a6691e5ba87642053faed55a0db3e448
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-06-06 11:05:41 +0530

    sum reduce

---
+++ sumReduceStream.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class sumReduceStream {
+    public static void main(String[] args) {
+        List<Integer> list =Arrays.asList(2,28,40,49,50);
+        Optional<Integer> sum = list.stream().reduce((a,b)->a+b);
+        System.out.println(sum);
+    }
+}
