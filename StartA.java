7cd0b0ef46d664f390a6f6a853b3381b459b81c8
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-06-09 11:06:48 +0530

    map

--- StartA.java
+++ StartA.java
@@ -1,11 +1,10 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StartA {
     public static void main(String[] args) {
         List<String> list =Arrays.asList("java","python","apple","javascript","Avagodo");
-        List<Integer> count = list.stream().filter(a->a.startsWith(a)).map(a->a.length()).collect(Collectors.toList());
-        System.out.println(count);
+        int countt = (int) list.stream().map(a->a.toLowerCase()).filter(a->a.charAt(0)=='a').count();
+        System.out.println(countt);
     }
 }
