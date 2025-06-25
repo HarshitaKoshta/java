e90d1174f0002e0f0df0341fc676457603efc7a2
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-06-25 23:23:52 +0530

    FirstNonRep

--- Practice/FirstNonRep.java
+++ Practice/FirstNonRep.java
@@ -3,17 +3,21 @@ import java.util.HashMap;
 
 public class FirstNonRep {
     public static void main(String[] args) {
-        int[] a = {1,3,4,9,1,6,5,3,9};
-     HashMap<Integer,Integer> map = new HashMap<>();
+        String s = "loveleetcode";
 
-     for(int n:a){
-        map.put(n, map.getOrDefault(n, 0)+1);
-     }
-     for(int n:a){
-        if(map.get(n)==1)
-{
-    System.out.println(n);
-    break;
-}     }
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                System.out.println("Index of first non-repeating character: " + i);
+                return;
+            }
+        }
+
+        System.out.println("-1 (No non-repeating character)");
     }
 }
